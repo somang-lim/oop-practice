3605b4995f04e53d700692e18c39367fb95a0c26
somang-lim <somangjuha@gmail.com>
2023-03-12 18:43:01 +0900

    Test: 비밀번호가 8자 미만 또는 12자 초과인 경우 IllegalArgumentException 예외가 발생한다

--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -20,4 +20,11 @@ public class PasswordValidatorTest {
 			.doesNotThrowAnyException();
 	}
 
+	@DisplayName("비밀번호가 8자 미만 또는 12자 초과인 경우 IllegalArgumentException 예외가 발생한다.")
+	@Test
+	void validatePasswordTest2() {
+		assertThatCode(() -> PasswordValidator.validate("aabb"))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
+	}
 }
