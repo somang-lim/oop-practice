7af108261f8fff299da7cb181f4c91d01f79673f
somang-lim <somangjuha@gmail.com>
2023-03-12 18:43:47 +0900

    Test: 경계조건에 대한 테스트 코드 추가(ParameterizedTest)

--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * 비밀번호는 최소 8자 이상 12자 이하여야 한다.
@@ -21,10 +23,12 @@ public class PasswordValidatorTest {
 	}
 
 	@DisplayName("비밀번호가 8자 미만 또는 12자 초과인 경우 IllegalArgumentException 예외가 발생한다.")
-	@Test
-	void validatePasswordTest2() {
-		assertThatCode(() -> PasswordValidator.validate("aabb"))
+	@ParameterizedTest
+	@ValueSource(strings = {"aabbcce", "aabbccddeeffg"})
+	void validatePasswordTest2(String password) {
+		assertThatCode(() -> PasswordValidator.validate(password))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
 	}
+
 }
