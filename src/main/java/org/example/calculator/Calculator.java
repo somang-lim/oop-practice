dfaf7d169e7785d6a2812dcc9a7def38583d5e85
somang-lim <somangjuha@gmail.com>
2023-03-13 23:45:58 +0900

    Test: 기본 사칙연산 2차 리팩토링(Interface 사용)

--- src/main/java/org/example/calculator/Calculator.java
+++ src/main/java/org/example/calculator/Calculator.java
@@ -1,7 +1,20 @@
 package org.example.calculator;
 
+import java.util.List;
+
+import org.example.calculator.operator.AdditionOperator;
+import org.example.calculator.operator.DivisionOperator;
+import org.example.calculator.operator.MultiplicationOperator;
+import org.example.calculator.operator.NewArithmeticOperator;
+import org.example.calculator.operator.SubtractionOperator;
+
 public class Calculator {
+	private static final List<NewArithmeticOperator> arithmeticOperators = List.of(new AdditionOperator(), new SubtractionOperator(), new MultiplicationOperator(), new DivisionOperator());
 	public static int calculator(int operand1, String operator, int operand2) {
-		return ArithmeticOperator.calculate(operand1, operator, operand2);
+		return arithmeticOperators.stream()
+			.filter(arithmeticOperator -> arithmeticOperator.supports(operator))
+			.map(arithmeticOperator -> arithmeticOperator.calculate(operand1, operand2))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
 	}
 }
