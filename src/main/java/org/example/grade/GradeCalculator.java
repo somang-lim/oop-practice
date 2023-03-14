43ca81081f4a78f496ae370ecaa1202087e4808a
somang-lim <somangjuha@gmail.com>
2023-03-14 20:31:06 +0900

    Refactor: 학점 계산기 2차 리팩토링

--- src/main/java/org/example/grade/GradeCalculator.java
+++ src/main/java/org/example/grade/GradeCalculator.java
@@ -9,12 +9,16 @@ public class GradeCalculator {
 		this.courses = new Courses(courses);
 	}
 
+	public GradeCalculator(Courses courses) {
+		this.courses = courses;
+	}
+
 	/**
-	 * 요구사항
-	 * 평균 학점 계산 방법 (학점수 * 교과목 평점) 의 합계 / 수강신청 총 학점 수
-	 * 일급 컬렉션 사용
+	 * 핵심 포인트
 	 */
-	public double calculateGradle() {
+	// 이수한 과목을 전달하여 평균 학점 계산 요청 ----> 학점 계산기 ---> (학점수 * 교과목 평점)의 합계 ---> 과목(코스) 일급 컬렉션
+	//                                                 ---> 수강신청 총 학점 수         ---> 과목(코스) 일급 컬렉션
+	public double calculateGrade() {
 		// (학점수 * 교과목 평점) 의 합계
 		double totalMultipliedCreditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
 
--- src/test/java/org/example/grade/GradeCalculatorTest.java
+++ src/test/java/org/example/grade/GradeCalculatorTest.java
@@ -16,6 +16,9 @@ public class GradeCalculatorTest {
 	// 학점 계산기 도메인 : 이수한 과목(객체지향프로그래밍, 자료구조, 스페인회화), 학점 계산기
 	// 객체지향프로그래밍, 자료구조, 스페인회화 --> 과목(코스) 클래스
 
+	/**
+	 * 핵심 포인트
+	 */
 	// 이수한 과목을 전달하여 평균 학점 계산 요청 ----> 학점 계산기 ---> (학점수 * 교과목 평점)의 합계 ---> 과목(코스)
 	//                                                 ---> 수강신청 총 학점 수         ---> 과목(코스)
 
@@ -25,8 +28,8 @@ public class GradeCalculatorTest {
 		List<Course> courses = List.of(new Course("OOP", 3, "A+"),
 			new Course("자료구조", 3, "A+"));
 
-		GradeCalculator gradeCalculator = new GradeCalculator(courses);
-		double gradeResult = gradeCalculator.calculateGradle();
+		GradeCalculator gradeCalculator = new GradeCalculator(new Courses(courses));
+		double gradeResult = gradeCalculator.calculateGrade();
 
 		assertThat(gradeResult).isEqualTo(4.5);
 	}
